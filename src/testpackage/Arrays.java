8c6619dbb70f20f0b9626c8b4aae6b52cb236dda
manuellima <manuellima@manuellima-Predator-G9-593>
2021-03-17 16:07:44 -0600

    Commiting Arrays classes. Task done.

---
+++ src/testpackage/Arrays.java
@@ -0,0 +1,25 @@
+package testpackage;
+
+import java.util.ArrayList;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String arrayOne[] = {"Manuel","Lima","Luna","Carolina"};
+		ArrayList<Integer> numbers = new ArrayList<Integer>(); 
+		numbers.add(12);
+		numbers.add(34);
+		numbers.add(14);
+		numbers.add(17);
+		for(int i = 0; i < arrayOne.length; i++) {
+			System.out.println("Word is: " + arrayOne[i]);
+			System.out.println(numbers.get(i));
+			
+		}
+		
+		
+
+	}
+
+}
