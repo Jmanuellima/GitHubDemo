22e581c7f93928a3d25b10946ac5aa10e888b28c
manuellima <manuellima@manuellima-Predator-G9-593>
2021-03-17 22:15:30 -0600

    Added another array list integer required.

--- src/testpackage/Arrays.java
+++ src/testpackage/Arrays.java
@@ -7,6 +7,7 @@ public class Arrays {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String arrayOne[] = {"Manuel","Lima","Luna","Carolina"};
+		int arrayTwo[] = {313,34,56,78};
 		ArrayList<Integer> numbers = new ArrayList<Integer>(); 
 		numbers.add(12);
 		numbers.add(34);
@@ -15,6 +16,7 @@ public class Arrays {
 		for(int i = 0; i < arrayOne.length; i++) {
 			System.out.println("Word is: " + arrayOne[i]);
 			System.out.println(numbers.get(i));
+			System.out.println(arrayTwo[i]);
 			
 		}
 		
